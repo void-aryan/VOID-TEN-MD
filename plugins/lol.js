bb1b6a0c32f8135b5d66f0b67f82d9b4c79f226f
INCONNU BOY <inconnuboy39@gmail.com>
2025-05-29 17:35:55 -0300

    Create lol.js

---
+++ plugins/lol.js
@@ -0,0 +1,26 @@
+const config = require('../config');
+const { cmd, commands } = require('../command');
+
+cmd({
+    pattern: "ping",
+    desc: "Check bot's response time.",
+    category: "main",
+    react: "🍂",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        const startTime = Date.now()
+        const message = await conn.sendMessage(from, { text: '*I AM YOUR QUEEN 👸...*' })
+        const endTime = Date.now();
+const ping = endTime - startTime;
+
+await conn.sendMessage(from, {
+    text: `
+╭━━〔 *📡 SPEED TEST* 〕━━╮
+┃ ⚙️ *Bot* : QUEEN ASUNA MD
+┃ ⚡ *Ping* : ${ping} ms
+┃ ⏱️ *Checked at* : ${moment().format("HH:mm:ss")}
+╰━━━━━━━━━━━━━━━━━━━━━━╯
+`.trim()
+}, { quoted: message });
